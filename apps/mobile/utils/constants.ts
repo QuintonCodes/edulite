c69ef024b3af14b4111a871d3e89dead8f85aad6
CodeQ09 <kagisojiyane28@gmail.com>
2025-11-17 10:31:51 +0200

    fix(build): added react-native-web-webview

--- apps/mobile/utils/constants.ts
+++ apps/mobile/utils/constants.ts
@@ -1,10 +1,3 @@
-import Constants from 'expo-constants';
-
-export const API_BASE = Constants.expoConfig?.extra?.API_BASE;
-
-if (!API_BASE) {
-  throw new Error("API_BASE is not configured in app.json's 'extra' field.");
-}
-
+export const API_BASE = 'https://edulite-backend-nu.vercel.app/api';
 export const ACCESS_KEY = 'accessToken';
 export const REFRESH_KEY = 'refreshToken';
