49277d57cf0e337f8c3e832f88849f0a9f260230
CodeQ09 <kagisojiyane28@gmail.com>
2025-11-17 09:51:09 +0200

    fix(build): added constant in app.json

--- apps/mobile/utils/constants.ts
+++ apps/mobile/utils/constants.ts
@@ -1,3 +1,10 @@
-export const API_BASE = 'https://edulite-backend-nu.vercel.app/api';
+import Constants from 'expo-constants';
+
+export const API_BASE = Constants.expoConfig?.extra?.API_BASE;
+
+if (!API_BASE) {
+  throw new Error("API_BASE is not configured in app.json's 'extra' field.");
+}
+
 export const ACCESS_KEY = 'accessToken';
 export const REFRESH_KEY = 'refreshToken';
